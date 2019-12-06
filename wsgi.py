6ee1f3caf10031b61d2f7eccb40f0aa6213c78dc
slombard2011 <47317573+slombard2011@users.noreply.github.com>
2019-12-06 16:03:22 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,32 @@
 from flask import Flask
-application = Flask(__name__)
+import psycopg2
+import os
+app = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+USER = os.environ.get("POSTGRESQL_USER")
+PASSWORD = os.environ.get("POSTGRESQL_PASSWORD")
+HOST = "127.0.0.1"
+PORT = "5432"
+DATABASE = os.environ.get("POSTGRESQL_DATABASE")
 
-if __name__ == "__main__":
-    application.run()
+@app.route('/')
+def health_check():
+    try:
+        connection = psycopg2.connect(user = USER,
+                                      password = PASSWORD,
+                                      host = HOST,
+                                      port = PORT,
+                                      database = DATABASE)
+        cursor = connection.cursor()
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+        return "Database connexion is ok "
+    except ( psycopg2.Error) as error :
+        return ('Unable to connect to the database!\n{0}').format(error)
+
+if __name__ == '__main__':
+    app.run()
